31851778a6ffe459b07c2550f9bf203861ff2b60
amitpadhan525 <amitpadhan525@gmail.com>
2026-07-12 15:31:17 +0530

    ADDED threading concept to run all monitors and file_monitors at a time

--- linux_eye/main.py
+++ linux_eye/main.py
@@ -1,11 +1,27 @@
-from linux_eye.monitors import process_monitor, network_monitor
+from linux_eye.monitors import process_monitor, network_monitor, file_monitor
 from linux_eye.utils.config import CONFIG
 import time
+import threading
 
-print("-------THIS IS STARTING-------")
 
-while True:
-    process_monitor.run()
-    network_monitor.run()
-    time.sleep(CONFIG['general']['refresh_interval'])
-    print('RUNNING')
+def monitor_loop():
+    while True:
+        process_monitor.run()
+        network_monitor.run()
+        time.sleep(CONFIG['general']['refresh_interval'])
+        print('RUNNING')
+
+def main():
+    print("------- LinuxEye Started -------")
+
+    t1 = threading.Thread(target=monitor_loop)
+    t2 = threading.Thread(target=file_monitor.run)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+if __name__ == "__main__":
+    main()
