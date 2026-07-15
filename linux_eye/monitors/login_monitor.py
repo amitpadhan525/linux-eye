c0ab70c6b509a29795bb79ae377e8e3d5cc159ed
amitpadhan525 <amitpadhan525@gmail.com>
2026-07-15 09:38:28 +0530

    Adding brute force checker ,if found call log_critical()

--- linux_eye/monitors/login_monitor.py
+++ linux_eye/monitors/login_monitor.py
@@ -1,6 +1,46 @@
 import subprocess
 from linux_eye.utils.logger import log_critical,log_info,log_warning
 process=subprocess.Popen(['journalctl','-f'],stdout=subprocess.PIPE,text=True)
+from collections import deque
+import time
+
+failed_logins={}
+WINDOW_SECONDS = 60
+THRESHOLD = 5
+def check_brute_force(line):
+    if 'pam_unix(sudo:auth): authentication failure' not in line:
+        return
+    
+    if 'user=' in line:
+        username=line.split('user=')[1].split(' ')[0]
+    else:
+        username=None
+
+    if username is None:
+        return
+    
+    now=time.time()
+    if username in failed_logins:
+        failed_logins[username].append(now)
+    else:
+        failed_logins[username]=deque([now])
+
+    while failed_logins[username] and now-failed_logins[username][0]>WINDOW_SECONDS:
+        failed_logins[username].popleft()
+
+   
+
+    if len(failed_logins[username])>=THRESHOLD:
+        log_critical(
+            source='Log monitor',
+            message=f"Multiple failed login attempt on username {username}.",
+            details={
+                "username": username,
+                "failure_count": len(failed_logins[username]),
+                "window_seconds": WINDOW_SECONDS
+                }
+        )
+
 
 def run():
     for line in process.stdout:
@@ -16,3 +56,4 @@ def run():
                 message="Wrong Password entred",
                 details=line
             )
+        check_brute_force(line)
